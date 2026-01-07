945a62d374096fbf18e075aaa4ff849be65cdf88
Abhay-k12 <abhaykanojia51@gmail.com>
2026-01-07 17:03:04 +0530

    Day 11 Completed

---
+++ Array/Day11_Maximum_Product_Subarray.java
@@ -0,0 +1,45 @@
+/*
+Given an array arr[] that contains positive and negative integers (may contain 0 as well). 
+Find the maximum product that we can get in a subarray of arr[].
+Note: It is guaranteed that the answer fits in a 32-bit integer.
+
+Examples
+Input: arr[] = [-2, 6, -3, -10, 0, 2]
+Output: 180
+Explanation: The subarray with maximum product is [6, -3, -10] with product = 6 * (-3) * (-10) = 180.
+
+Input: arr[] = [-1, -3, -10, 0, 6]
+Output: 30
+Explanation: The subarray with maximum product is [-3, -10] with product = (-3) * (-10) = 30.
+
+Input: arr[] = [2, 3, 4] 
+Output: 24 
+Explanation: For an array with all positive elements, the result is product of all elements. 
+
+Constraints:
+1 ≤ arr.size() ≤ 10^6
+-10  ≤  arr[i]  ≤  10
+*/
+package Array;
+
+class Solution {
+    int maxProduct(int[] arr) {
+        int maxProduct = Integer.MIN_VALUE;
+        int currProduct = 1;
+        
+        for(int i=0; i<arr.length; i++) {
+            currProduct *= arr[i];
+            maxProduct = Math.max(currProduct, maxProduct);
+            if(currProduct==0) currProduct=1;
+        }
+        
+        currProduct=1;
+        for(int i=arr.length-1; i>=0; i--) {
+            currProduct *= arr[i];
+            maxProduct = Math.max(currProduct, maxProduct);
+            if(currProduct==0) currProduct=1;
+        }
+        
+        return maxProduct;
+    }
+}
